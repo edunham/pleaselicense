01cdf750ce9a9dbcc91f72398db3f0f0018fc6dc
edunham <edunham@osuosl.org>
2015-02-10 12:53:02 -0800

    allow user to enter username to search

--- gh.js
+++ gh.js
@@ -5,7 +5,12 @@
 function handleRepoList() {
     var data = JSON.parse(this.responseText);
     console.log(data);
-    data.forEach(learnAboutRepo);
+    if (data.message && data.message.match(/Not Found/)){
+        alert("Invalid user.");
+    }
+    else{
+        data.forEach(learnAboutRepo);
+    }
 }
 function learnAboutRepo(repoObj){
     var name = repoObj.name;
@@ -13,6 +18,7 @@ function learnAboutRepo(repoObj){
     var url = repoObj.url + '/contents';
     console.log(url);
     var repoReq = new XMLHttpRequest();
+    //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/bind
     repoReq.onload = digInFiles.bind(repoReq, name);
     repoReq.open("get", url, true);
     repoReq.send();
@@ -23,15 +29,21 @@ function digInFiles(name){
     var repo = JSON.parse(this.responseText)
     var found = false;
     console.log(repo);
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
     repo.reduce(function(old, f, idx, array){
+        // !! casts to a boolean. the /i on match makes it case insensitive.
         found = !!(found || f.name.match(/license/i) || f.name.match(/copying/i));
     });
     console.log(found);
 }
 
-console.log("don't worry, it's working");
-var oReq = new XMLHttpRequest();
-oReq.onload = handleRepoList;
-oReq.open("get", "https://api.github.com/users/marajade/repos", true);
-oReq.send();
-
+function getUser(){
+    //this function called by clicking the stalk repos button
+    var user = document.getElementById('ghuser').value;
+    var oReq = new XMLHttpRequest();
+    oReq.onload = handleRepoList;
+    var url = "https://api.github.com/users/" + user + "/repos";
+    oReq.open("get", url, true);
+    oReq.send();
+    console.log("username " + user);
+}
