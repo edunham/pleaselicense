85252086c8c1c3e2fdfbe2f5f852c3cef97e7079
edunham <edunham@osuosl.org>
2015-02-10 14:58:19 -0800

    it works!

--- gh.js
+++ gh.js
@@ -16,15 +16,16 @@ function learnAboutRepo(repoObj){
     var name = repoObj.name;
     console.log(name);
     var url = repoObj.url + '/contents';
+    var html_url = repoObj.html_url;
     console.log(url);
     var repoReq = new XMLHttpRequest();
     //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/bind
-    repoReq.onload = digInFiles.bind(repoReq, name);
+    repoReq.onload = digInFiles.bind(repoReq, name, html_url);
     repoReq.open("get", url, true);
     repoReq.send();
 }
 
-function digInFiles(name){
+function digInFiles(name, link){
     console.log(name);
     var repo = JSON.parse(this.responseText)
     var found = false;
@@ -34,11 +35,23 @@ function digInFiles(name){
         // !! casts to a boolean. the /i on match makes it case insensitive.
         found = !!(found || f.name.match(/license/i) || f.name.match(/copying/i));
     });
+    var append = "<li>"+"<a href=\""+link+"\">"+name+"</a></li>";
     console.log(found);
+    console.log(append);
+    if (found){
+        document.getElementById("goodrepos").innerHTML += append;
+    }
+    else{
+        document.getElementById("badrepos").innerHTML += append;
+    }
 }
 
 function getUser(){
     //this function called by clicking the stalk repos button
+    //first, clear any old results
+    document.getElementById("goodrepos").innerHTML = "";
+    document.getElementById("badrepos").innerHTML = "";
+    // find the username being searched, and send request
     var user = document.getElementById('ghuser').value;
     var oReq = new XMLHttpRequest();
     oReq.onload = handleRepoList;
@@ -46,4 +59,7 @@ function getUser(){
     oReq.open("get", url, true);
     oReq.send();
     console.log("username " + user);
+    //make output visible and hide intsructions
+    document.getElementById("results").style.display = 'block';
+    document.getElementById("instructions").style.display = 'none';
 }
