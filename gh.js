bb70c8500e0c403030c8dc6f5f64f512ff048de5
Euan <euank@euank.com>
2015-10-23 23:39:30 -0700

    Allow user to input a github API token
    Also some minor code cleanup; small spacing stuff, wrapping function,
    all that jazz.

--- gh.js
+++ gh.js
@@ -1,3 +1,4 @@
+(function() {
 "use strict";
 function rateMessage(){
     console.log("rate limit fail");
@@ -6,7 +7,7 @@ function rateMessage(){
     document.getElementById("instructions").style.display = 'block';
     var message = "It looks like you've exceeded the GitHub API's rate"+
     "limit, which is 60 requests per hour. Try again later, or from a "+
-    "different IP address.";
+    'different IP address. Alternately, enter a token (as found <a href="https://github.com/settings/tokens">here</a>"';
     document.getElementById("instructions").innerHTML = message;
 }
 // yay https://developer.github.com/v3/#cross-origin-resource-sharing
@@ -18,15 +19,23 @@ function handleRepoList() {
         if (data.message.match(/Not Found/)){
             alert("Invalid user. Please try with a valid GitHub username.");
         }
-        if (data.message.match(/API rate limit/i)){
+        else if (data.message.match(/API rate limit/i)){
             rateMessage();
         }
+        else {
+            // make sure an unknown error doesn't get lost in the aether
+            var row = document.createElement("li");
+            row.appendChild(document.createTextNode(data.message));
+            document.getElementById("messages").appendChild(row);
+        }
+        // TODO, handle 'Bad credentials' more gracefully
     }
     else{
         data.forEach(learnAboutRepo);
     }
 }
 function learnAboutRepo(repoObj){
+    var token = document.getElementById("ghtoken").value;
     var name = repoObj.name;
     console.log(name);
     var url = repoObj.url + '/contents';
@@ -36,6 +45,9 @@ function learnAboutRepo(repoObj){
     //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/bind
     repoReq.onload = digInFiles.bind(repoReq, name, html_url);
     repoReq.open("get", url, true);
+    if(token.length > 0) {
+        repoReq.setRequestHeader("Authorization", "token " + token);
+    }
     repoReq.send();
 }
 
@@ -43,14 +55,15 @@ function digInFiles(name, link){
     console.log(name);
     var repo = JSON.parse(this.responseText);
     var licensefound = false;
-    var readmefound = false; 
+    var readmefound = false;
     repo.forEach(function(repo){
         licensefound = !!(licensefound || repo.name.match(/license/i) || repo.name.match(/copying/i));
-        readmefound = !!(readmefound || repo.name.match(/readme/i))
+        readmefound = !!(readmefound || repo.name.match(/readme/i));
     });
     console.log(repo);
     var append = "";
     if (repo.message){
+        // If we got back a message, that means we hit an error of some kind. Add it to the 'messages' thingy just so the user sees *something*
         if (repo.message.match(/API rate limit/i)){
             rateMessage();
         }
@@ -60,7 +73,7 @@ function digInFiles(name, link){
         }
     }
     else{
-        // try to do analytics with gaq 
+        // try to do analytics with gaq
         var _gaq = _gaq || [];
         append = "<li>"+"<a href=\""+link+"\">"+name+"</a></li>";
         if (licensefound){
@@ -88,6 +101,7 @@ function getUser(){
     document.getElementById("haslicense").innerHTML = "";
     document.getElementById("lackslicense").innerHTML = "";
     var user = document.getElementById('ghuser').value;
+    var token = document.getElementById("ghtoken").value;
 
     // be stalkey, because why not
     var _gaq = _gaq || [];
@@ -98,6 +112,9 @@ function getUser(){
     oReq.onload = handleRepoList;
     var url = "https://api.github.com/users/" + user + "/repos";
     oReq.open("get", url, true);
+    if(token.length > 0) {
+        oReq.setRequestHeader("Authorization", "token " + token);
+    }
     oReq.send();
     console.log("username " + user);
     //make output visible and hide intsructions
@@ -110,3 +127,5 @@ document.querySelector('#ghform').addEventListener('submit', function(ev){
     getUser();
     return false;
 });
+
+})();
