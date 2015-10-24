1b97e973aea3cf70be8682a1dd56bc6430a930c0
Euan <euank@euank.com>
2015-10-23 23:40:08 -0700

    Add option to hide forks (default on)
    Someone isn't typically directly responsible for the license / readme of
    a fork of a project; they might be, but they can uncheck the box to see
    those still.
    
    This should help with signal-to-noise ratio a little bit

--- gh.js
+++ gh.js
@@ -40,10 +40,11 @@ function learnAboutRepo(repoObj){
     console.log(name);
     var url = repoObj.url + '/contents';
     var html_url = repoObj.html_url;
+    var isfork = repoObj.fork;
     console.log(url);
     var repoReq = new XMLHttpRequest();
     //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/bind
-    repoReq.onload = digInFiles.bind(repoReq, name, html_url);
+    repoReq.onload = digInFiles.bind(repoReq, name, isfork, html_url);
     repoReq.open("get", url, true);
     if(token.length > 0) {
         repoReq.setRequestHeader("Authorization", "token " + token);
@@ -51,7 +52,7 @@ function learnAboutRepo(repoObj){
     repoReq.send();
 }
 
-function digInFiles(name, link){
+function digInFiles(name, isfork, link){
     console.log(name);
     var repo = JSON.parse(this.responseText);
     var licensefound = false;
@@ -61,36 +62,60 @@ function digInFiles(name, link){
         readmefound = !!(readmefound || repo.name.match(/readme/i));
     });
     console.log(repo);
-    var append = "";
+
+    // Wrap row creation in a function so that we get two different elements; can't append the same element twice
+    // cloneNode(deep) isn't supported on some old browsers I think, so avoid that
+    var createRow = function() {
+      var row = document.createElement("li");
+      var rowLink = document.createElement("a");
+      rowLink.href = link;
+      rowLink.appendChild(document.createTextNode(name));
+      row.appendChild(rowLink);
+
+      if (isfork) {
+        var isForkNode = document.createElement("i");
+        isForkNode.appendChild(document.createTextNode("(fork)"));
+        row.appendChild(isForkNode);
+
+        var forksHidden = document.querySelector("#hideforks").checked;
+        row.classList.add('fork');
+        if(forksHidden) {
+          row.classList.add('hide-fork');
+        }
+      }
+      return row;
+    };
+
     if (repo.message){
         // If we got back a message, that means we hit an error of some kind. Add it to the 'messages' thingy just so the user sees *something*
         if (repo.message.match(/API rate limit/i)){
             rateMessage();
         }
         else{
-            append = "<li>"+"<a href=\""+link+"\">"+name+"</a>"+repo.message+"</li>";
-            document.getElementById("messages").innerHTML += append;
+            var row = createRow();
+            row.appendChild(document.createTextNode(repo.message));
+            document.getElementById("messages").appendChild(row);
         }
     }
     else{
         // try to do analytics with gaq
         var _gaq = _gaq || [];
-        append = "<li>"+"<a href=\""+link+"\">"+name+"</a></li>";
+
         if (licensefound){
-            _gaq.push(['users._trackEvent', 'licenseFound', link])
-            document.getElementById("haslicense").innerHTML += append;
+            _gaq.push(['users._trackEvent', 'licenseFound', link]);
+            document.getElementById("haslicense").appendChild(createRow());
         }
         else{
-            _gaq.push(['users._trackEvent', 'licenseMissing', link])
-            document.getElementById("lackslicense").innerHTML += append;
+            _gaq.push(['users._trackEvent', 'licenseMissing', link]);
+            document.getElementById("lackslicense").appendChild(createRow());
         }
         if (readmefound){
-            _gaq.push(['users._trackEvent', 'readmeFound', link])
-            document.getElementById("hasreadme").innerHTML += append;
+            _gaq.push(['users._trackEvent', 'readmeFound', link]);
+            document.getElementById("hasreadme").appendChild(createRow());
         }
         else{
-            _gaq.push(['users._trackEvent', 'readmeMissing', link])
-            document.getElementById("lacksreadme").innerHTML += append;
+            _gaq.push(['users._trackEvent', 'readmeMissing', link]);
+            document.getElementById("lacksreadme").appendChild(createRow());
         }
     }
 }
@@ -106,7 +131,7 @@ function getUser(){
     // be stalkey, because why not
     var _gaq = _gaq || [];
     _gaq.push(['users._setAccount', 'UA-58732341-2']);
-    _gaq.push(['users._trackEvent', 'userChecked', user])
+    _gaq.push(['users._trackEvent', 'userChecked', user]);
 
     var oReq = new XMLHttpRequest();
     oReq.onload = handleRepoList;
@@ -126,6 +151,18 @@ document.querySelector('#ghform').addEventListener('submit', function(ev){
     ev.preventDefault();
     getUser();
     return false;
+});
+
+document.querySelector("#hideforks").addEventListener('change', function(ev) {
+  var forkNodes = document.querySelectorAll(".fork");
+  for(var i = 0; i < forkNodes.length; i++) {
+    if (ev.target.checked) {
+      forkNodes[i].classList.add("hide-fork");
+    }
+    else {
+      forkNodes[i].classList.remove("hide-fork");
+    }
+  }
 });
 
 })();
